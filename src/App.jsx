ab2aae6988ba4c92b7df7c5f5a2fa6727933eb10
CongNhut1210 <nhuttran12102000@gmail.com>
2025-09-10 10:17:57 +0700

    z

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  return (
+    
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Hello
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
+
